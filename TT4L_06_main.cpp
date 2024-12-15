13b8c551b94772551e20c2b21d7a3e5819b58c91
Brian Ng <144977561+brianngzhengyang@users.noreply.github.com>
2024-12-15 13:41:14 +0800

    Added main cpp file

---
+++ TT4L_06_main.cpp
@@ -0,0 +1,17 @@
+ // *********************************************************
+// Program: TT4L_06_main.cpp
+// Course: CCP6114 Programming Fundamentals
+// Lecture Class: TC2L
+// Tutorial Class: TT4L
+// Trimester: 2430
+// Member_1: ID | MEOR HAZIMI BIN MEOR MOHAMMAD FARED | EMAIL@student.mmu.edu.my  | PHONE
+// Member_2: 242UC244RD | BRIAN NG ZHENG YANG | BRIAN.NG.ZHENG@student.mmu.edu.my | 017-7793199
+// Member_3: ID | ALIF AKMAL BIN ABDUL HALIM | EMAIL@student.mmu.edu.my  | PHONE
+// Member_4: ID | MOHAMMED AMAN | EMAIL@student.mmu.edu.my  | PHONE
+// *********************************************************
+// Task Distribution
+// Member_1:
+// Member_2:
+// Member_3:
+// Member_4:
+// *********************************************************
